88af672290abd633349c7423f27651af42a5bf10
Md Abu Sayed Islam <abusayediit@gmail.com>
2021-01-16 19:30:19 +0600

    Auto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'LearningApp';
+export class AppComponent implements OnInit {
+  data:any;
+ 
+  title = 'Hellow World';
+  height :number=30;
+num1:number;
+num2:number;
+result:number;
+sub:number;
+
+add(){
+  this.result=this.num1+this.num2;
+}
+
+substract(){
+  this.sub=this.num1-this.num2;
+}
+
+  ngOnInit(): void {
+    this.data=this.getData();
+  }
+
+  getData(){
+    return [
+      
+      {"_id":"1",
+      "title":"title 1",
+      "body":"body 1"},
+      {"_id":"2",
+      "title":"title 2",
+      "body":"body 2"},
+      {"_id":"3",
+      "title":"title 3",
+      "body":"body 3"}
+  ]
+  }
 }
---
+++ src/app/second.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector:'app-second',
+   // templateUrl:'./second.component.html',
+    template:`<h1>Hellow World</h1>`,
+    styleUrls:['./second.component.css']
+})
+export class SecondComponent{
+
+}
